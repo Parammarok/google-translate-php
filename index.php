42b414dc8323522d5466d8d864466fab8acdf069
ParaMMarok <rockingboy3600@gmail.com>
2022-07-23 08:24:16 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ignore_user_abort(true);
+ini_set('default_socket_timeout', 5);
+error_reporting(E_ALL);
+ini_set("memory_limit", "4500M");
+set_time_limit(0);
+
+
+
+
+namespace Stichoza\GoogleTranslate\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Stichoza\GoogleTranslate\GoogleTranslate;
+
+$tr = new GoogleTranslate(); // Translates to 'en' from auto-detected language by default
+$tr->setSource('nl'); // Translate from English
+$tr->setSource(); // Detect language automatically
+$tr->setTarget('en'); // Translate to Georgian
+
+echo $tr->translate('Google wordt geblokkeerd in pro-Russische gebieden in Oekraïne');
