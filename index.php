dd250463ad1e4fc7f3912d1f8966284846ea8edf
ParaMMarok <rockingboy3600@gmail.com>
2022-08-20 08:26:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 require_once ('vendor/autoload.php');
 use Stichoza\GoogleTranslate\GoogleTranslate;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-ignore_user_abort(true);
-ini_set('default_socket_timeout', 5);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//ini_set('default_socket_timeout', 5);
 error_reporting(E_ALL);
 ini_set("memory_limit", "4500M");
-set_time_limit(0);
+ini_set('max_execution_time', '9');
+set_time_limit(10);
 $tr = new GoogleTranslate(); // Translates to 'en' from auto-detected language by default
 $from = $_REQUEST['from'];
 $to= $_REQUEST['to'];
